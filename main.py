d67a4b675a4f8fea878aad6265dcd1af238549a3
johanna <johanna.kull@live.de>
2024-12-23 17:13:04 +0000

    refactor to split code into functions

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import requests
+from datetime import datetime as dt
 
 MY_LATITUDE = 51.507351
 MY_LONGITUDE = -0.127758
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-#
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-#
-# iss_position = (longitude, latitude)
 
-parameters = {
-    "lat": MY_LATITUDE,
-    "lng": MY_LONGITUDE,
-}
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+def get_current_iss_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_longitude = data["iss_position"]["longitude"]
+    iss_latitude = data["iss_position"]["latitude"]
+
+    return iss_longitude, iss_latitude
+
+
+def get_sunrise_and_sunset_hours():
+    parameters = {
+        "lat": MY_LATITUDE,
+        "lng": MY_LONGITUDE,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise_hour = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset_hour = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+    return sunrise_hour, sunset_hour
+
+
+iss_position = get_current_iss_position()
+
+current_time = dt.now()
