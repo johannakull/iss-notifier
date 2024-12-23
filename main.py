ca8ff8af4abfd9151c7fc9336eaad5eebfd42064
johanna <johanna.kull@live.de>
2024-12-23 15:20:30 +0000

    in progress - get sunrise and sunset times for my location

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LATITUDE = 51.507351
+MY_LONGITUDE = -0.127758
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+
+parameters = {
+    "lat": MY_LATITUDE,
+    "lng": MY_LONGITUDE,
+}
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
+print(data)
